236ff6400f0290dfa7346b73de71d91e21e58681
= <ios.dev.martins@gmail.com>
2022-09-11 19:19:16 +0200

    Added TextField on HomeVC

--- MoMovie/Home/View/HomeVC.swift
+++ MoMovie/Home/View/HomeVC.swift
@@ -1,12 +1,29 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    
+    private var searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter text here"
+        textField.font = UIFont.systemFont(ofSize: 15)
+        textField.textColor = .dynamicColor(light: .black, dark: Colors.sand)
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        textField.autocorrectionType = UITextAutocorrectionType.no
+        textField.keyboardType = UIKeyboardType.default
+        textField.returnKeyType = UIReturnKeyType.search
+        textField.clearButtonMode = UITextField.ViewMode.whileEditing
+        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Colors.main
         self.title = "Home"
         print("HomePage loadded")
     }
+    
+    
 }
 
