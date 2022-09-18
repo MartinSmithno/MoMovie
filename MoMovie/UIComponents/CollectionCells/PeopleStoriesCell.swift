e691796eb495217e1502061df7ae1ce87ed0d6b5
= <ios.dev.martins@gmail.com>
2022-09-18 12:56:35 +0200

    Added storiesCell

---
+++ MoMovie/UIComponents/CollectionCells/PeopleStoriesCell.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+final class peopleStoriesCell: UICollectionViewCell {
+    
+    static let id = "PeopleStoriesCell"
+    
+    private let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        guard let image = UIImage(named: "tom-hardy") else {
+            imageView.image = UIImage(named: "profile-default")!
+            return imageView
+        }
+        imageView.image = image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addViews() {
+        profileImage.fill(parentView: self)
+    }
+}
